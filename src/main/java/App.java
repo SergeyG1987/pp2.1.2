f997fc76fc315bb2a95764451a2ecd721f6e0968
Bolotov Alexander <distalker1@gmail.com>
2022-01-18 08:42:56 +0300

    Update 20.06.25

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,17 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+        System.out.println(bean==bean1);
+        Cat cat =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getName());
+        Cat cat1 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(cat1.getName());
+        System.out.println(cat==cat1);
+
     }
 }
